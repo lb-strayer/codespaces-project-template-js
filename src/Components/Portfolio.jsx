b7d5815ae2dedf375e3646cec8551a3b26b88ca1
lb-strayer <94737781+lb-strayer@users.noreply.github.com>
2023-08-17 22:15:13 +0000

    update background  photo for Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/artificial-intelligence.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Artificial Intelligence";
 
 /**
  * Project list
