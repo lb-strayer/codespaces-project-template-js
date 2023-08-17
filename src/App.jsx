19a31aceeb2e260a47bb5aefa8bdd77a905451db
lb-strayer <94737781+lb-strayer@users.noreply.github.com>
2023-08-17 21:43:57 +0000

    update App.jsx with customized info.

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Lecrisha Bryan",
+  title: "Developer & Content Creator",
+  email: "lecrisha.bryan@my.strayer.edu",
+  gitHub: "lb-strayer",
+  instagram: "@HellraiserBarbie",
+  linkedIn: "lecrisha",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@HellraiserBarbi",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#5A1073";
+const secondaryColor = "#F72CE6";
 
 const App = () => {
   return (
